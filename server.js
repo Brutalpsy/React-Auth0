5540280a1a05b5a774b79d2d82b5fe55426f2829
psy <brutalpsy89@gmail.com>
2019-07-24 21:32:40 +0200

    changed to single quotes

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 checkRole = role => {
   return (req, res, next) => {
-    const assignedRoles = req.user["http://localhost:3000/roles"];
+    const assignedRoles = req.user['http://localhost:3000/roles'];
     if (Array.isArray(assignedRoles) && assignedRoles.includes(role)) {
       return next();
     } else {
-      return res.status(401).send("Insufficieent role");
+      return res.status(401).send('Insufficieent role');
     }
   };
 };
 
-var express = require("express");
+var express = require('express');
 var app = express();
-var jwt = require("express-jwt");
-var jwks = require("jwks-rsa");
-const checkScope = require("express-jwt-authz");
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+const checkScope = require('express-jwt-authz');
 
 var port = process.env.PORT || 8080;
 
@@ -22,36 +22,36 @@ var jwtCheck = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: "https://reactjscourse-dev.auth0.com/.well-known/jwks.json"
+    jwksUri: 'https://reactjscourse-dev.auth0.com/.well-known/jwks.json'
   }),
-  audience: "http://localhost:3001",
-  issuer: "https://reactjscourse-dev.auth0.com/",
-  algorithms: ["RS256"]
+  audience: 'http://localhost:3001',
+  issuer: 'https://reactjscourse-dev.auth0.com/',
+  algorithms: ['RS256']
 });
 
 app.use(jwtCheck);
 
-app.get("/public", (req, res) => {
+app.get('/public', (req, res) => {
   res.json({
-    message: "Hellow froma public API"
+    message: 'Hellow froma public API'
   });
 });
 
-app.get("/private", jwtCheck, (req, res) => {
+app.get('/private', jwtCheck, (req, res) => {
   res.json({
-    message: "Hellow froma PRIVATE API"
+    message: 'Hellow froma PRIVATE API'
   });
 });
 
-app.get("/course", jwtCheck, checkScope(["read:courses"]), (req, res) => {
+app.get('/course', jwtCheck, checkScope(['read:courses']), (req, res) => {
   res.json({
-    courses: [{ id: 1, title: "prvi title" }, { id: 2, title: "drugi title" }]
+    courses: [{ id: 1, title: 'prvi title' }, { id: 2, title: 'drugi title' }]
   });
 });
 
-app.get("/admin", jwtCheck, checkRole("admin"), (req, res) => {
+app.get('/admin', jwtCheck, checkRole('admin'), (req, res) => {
   res.json({
-    message: "Hellow from a admin API"
+    message: 'Hellow from a admin API'
   });
 });
 
